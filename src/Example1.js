af58919655431dceea59a14a99243b98c657f015
Soliu-Ahmad <102731648+Soliu-Ahmad@users.noreply.github.com>
2023-11-21 15:51:50 +0100

    first-commit

---
+++ src/Example1.js
@@ -0,0 +1,29 @@
+import { getPostsPage } from './api/axios'
+import { useState, useEffect } from 'react'
+import Post from './Post'
+
+const Example1 = () => {
+    const [page, setPage] = useState(1)
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+       getPostsPage(page).then((json) => setPosts(json));
+    }, [page]);
+
+
+    const nextPage = () => setPage(prev => prev + 1)
+
+    const prevPage = () => setPage(prev => prev - 1)
+
+    return (
+        <>
+            <nav>
+                <button onClick={prevPage} disabled={page === 1}>Prev Page</button>
+                <button onClick={nextPage} disabled={!posts.length}>Next Page</button>
+            </nav>
+            {posts.map(post => <Post key={post.id} post={post} />)
+}
+        </>
+    )
+}
+export default Example1
